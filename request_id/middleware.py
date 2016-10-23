3340d27e7a2571abaa341df950c48616722cf21b
Filip Wasilewski <en@ig.ma>
2016-10-23 03:37:57 +0200

    Make the middleware compatible with Django 1.10

--- request_id/middleware.py
+++ request_id/middleware.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
@@ -7,16 +7,30 @@ from .conf import settings
 
 
 def get_request_id(request):
-    if hasattr(request, "request_id"):
+    if hasattr(request, 'request_id'):
         return request.request_id
     elif settings.REQUEST_ID_HEADER:
-        return request.META.get(settings.REQUEST_ID_HEADER, "")
+        return request.META.get(settings.REQUEST_ID_HEADER, '')
     else:
         return generate_request_id()
 
 
 class RequestIdMiddleware(object):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
 
+    def __call__(self, request):
+        request_id = get_request_id(request)
+        request.request_id = request_id
+        local.request_id = request_id
+
+        response = self.get_response(request)
+
+        release_local(local)
+
+        return response
+
+    # Compatibility methods for Django <1.10
     def process_request(self, request):
         request_id = get_request_id(request)
         request.request_id = request_id
