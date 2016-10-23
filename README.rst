fe54d295dc45e51a0de368a7151f5ff72d0fb0c3
Filip Wasilewski <en@ig.ma>
2016-10-23 03:42:55 +0200

    Update demo and readme

--- README.rst
+++ README.rst
@@ -2,17 +2,18 @@
 django-request-id
 =================
 
-.. image:: https://pypip.in/v/django-request-id/badge.png
-    :target: https://pypi.python.org/pypi/django-request-id/
-    :alt: Latest Version
 
-.. image:: https://pypip.in/d/django-request-id/badge.png
-    :target: https://pypi.python.org/pypi/django-request-id/
-    :alt: Downloads
-
-.. image:: https://pypip.in/license/django-request-id/badge.png
-    :target: https://pypi.python.org/pypi/django-request-id/
+.. image:: https://img.shields.io/pypi/l/dj-cmd.svg
+    :target: https://raw.githubusercontent.com/nigma/django-request-id/master/LICENSE
     :alt: License
+
+.. image:: https://img.shields.io/pypi/v/django-request-id.svg
+    :target: https://pypi.python.org/pypi/django-request-id/
+    :alt: Latest PyPI version
+
+.. image:: https://img.shields.io/pypi/wheel/django-request-id.svg
+    :target: https://pypi.python.org/pypi/django-request-id/
+    :alt: Supports Wheel format
 
 Augments each request with unique ``request_id`` attribute and provides
 request id logging helpers.
@@ -28,8 +29,8 @@ Quickstart
 2. Add ``request_id`` to ``INSTALLED_APPS`` (necessary only if you are
    going to use the ``{% request_id %}`` template tag).
 
-3. Add ``request_id.middleware.RequestIdMiddleware`` to the top of
-   ``MIDDLEWARE_CLASSES``.
+3. Add ``request_id.middleware.RequestIdMiddleware`` at the top of
+   the ``MIDDLEWARE`` Django setting.
 
 4. The app integrates with the standard Python/Django logging by defining
    a filter that puts a ``request_id`` variable in scope of every log message.
@@ -103,15 +104,15 @@ Quickstart
 5. Make sure that your web server adds a ``X-Request-ID`` header to each request
    (and logs it in the server log for further matching of the server and app log entries).
 
-   Heroku handles this `automatically <https://devcenter.heroku.com/articles/http-request-id>`_.
-   On Nginx you may require a separate module (see
-   `nginx_requestid <https://github.com/hhru/nginx_requestid>`_ or
-   `nginx-x-rid-header <https://github.com/newobj/nginx-x-rid-header>`_).
-   On Apache you need to ``a2enmod`` the `unique_id <https://httpd.apache.org/docs/2.4/mod/mod_unique_id.html>`_
-   module and set ``REQUEST_ID_HEADER = "UNIQUE_ID"`` in the Django project
-   settings.
+   - Heroku handles this `automatically <https://devcenter.heroku.com/articles/http-request-id>`_.
+   - On Nginx you may require a separate module (see
+     `nginx_requestid <https://github.com/hhru/nginx_requestid>`_ or
+     `nginx-x-rid-header <https://github.com/newobj/nginx-x-rid-header>`_).
+   - On Apache you need to ``a2enmod`` the `unique_id <https://httpd.apache.org/docs/2.4/mod/mod_unique_id.html>`_
+     module and set ``REQUEST_ID_HEADER = "UNIQUE_ID"`` in the Django project
+     settings.
 
-   If you can't generate the X-Request-Id header at the web server level then
+   If you can't generate the `X-Request-Id` header at the web server level then
    simply set ``REQUEST_ID_HEADER = None`` in your project settings and the
    app will generate a unique id value automatically instead of retrieving
    it from the wsgi environment.
@@ -122,7 +123,7 @@ Quickstart
 Dependencies
 ------------
 
-``django-request-id`` depends on ``django-appconf>=0.6``.
+None.
 
 Documentation
 -------------
--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 #!/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 Demo script. Run:
@@ -12,94 +12,104 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 import os
 
+import django
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 
 basename = os.path.splitext(os.path.basename(__file__))[0]
+
+middleware_settings_name = 'MIDDLEWARE' if django.VERSION[:2] >= (1, 10) else 'MIDDLEWARE_CLASSES'
 
 
 def rel(*path):
     return os.path.abspath(
         os.path.join(os.path.dirname(__file__), *path)
-    ).replace("\\", "/")
+    ).replace('\\', '/')
 
 
 if not settings.configured:
     settings.configure(
         DEBUG=True,
-        TIMEZONE="UTC",
-        INSTALLED_APPS=["request_id"],
-        MIDDLEWARE_CLASSES=["request_id.middleware.RequestIdMiddleware"],
+        TIMEZONE='UTC',
+        INSTALLED_APPS=['request_id'],
         ROOT_URLCONF=basename,
-        WSGI_APPLICATION="{}.application".format(basename),
-        TEMPLATE_DIRS=[rel("tests", "templates")],
-        TEMPLATE_CONTEXT_PROCESSORS=[
-            "django.core.context_processors.request"
+        WSGI_APPLICATION='{}.application'.format(basename),
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [rel('tests', 'templates')],
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.request'
+                    ],
+                },
+            },
         ],
         LOGGING={
-            "version": 1,
-            "disable_existing_loggers": False,
-            "filters": {
-                "request_id": {
-                    "()": "request_id.logging.RequestIdFilter"
+            'version': 1,
+            'disable_existing_loggers': False,
+            'filters': {
+                'request_id': {
+                    '()': 'request_id.logging.RequestIdFilter'
                 }
             },
-            "formatters": {
-                "console": {
-                    "format": "%(asctime)s - %(levelname)-5s [%(name)s] request_id=%(request_id)s %(message)s",
-                    "datefmt": "%H:%M:%S"
+            'formatters': {
+                'console': {
+                    'format': '%(asctime)s - %(levelname)-5s [%(name)s] request_id=%(request_id)s %(message)s',
+                    'datefmt': '%H:%M:%S'
                 }
             },
-            "handlers": {
-                "console": {
-                    "level": "DEBUG",
-                    "filters": ["request_id"],
-                    "class": "logging.StreamHandler",
-                    "formatter": "console"
+            'handlers': {
+                'console': {
+                    'level': 'DEBUG',
+                    'filters': ['request_id'],
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'console'
                 }
             },
-            "loggers": {
-                "": {
-                    "level": "DEBUG",
-                    "handlers": ["console"]
+            'loggers': {
+                '': {
+                    'level': 'DEBUG',
+                    'handlers': ['console']
                 }
             }
+        },
+        **{
+            middleware_settings_name: ['request_id.middleware.RequestIdMiddleware']
         }
     )
-
 
 # App
 
 from django.views.generic.base import TemplateView
 from request_id import get_current_request_id
 
-logger = logging.getLogger("view")
+logger = logging.getLogger('view')
 
 
 class HelloView(TemplateView):
-    template_name = "base.html"
+    template_name = 'base.html'
 
     def get(self, request, *args, **kwargs):
-        logger.info("handling request")
+        logger.info('handling request')
         return super(HelloView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        logger.info("preparing context data")
+        logger.info('preparing context data')
         return super(HelloView, self).get_context_data(
             current_request_val=get_current_request_id(),
             **kwargs
         )
 
     def render_to_response(self, context, **response_kwargs):
-        logger.info("rendering template")
+        logger.info('rendering template')
         return super(HelloView, self).render_to_response(context, **response_kwargs)
 
 
-urlpatterns = patterns("",
-    url(r"^$", HelloView.as_view())
-)
-
+urlpatterns = [
+    url(r'^$', HelloView.as_view())
+]
 
 # WSGI
 
@@ -107,6 +117,7 @@ from request_id.wsgi import AddRequestIdHeaderMiddleware
 
 application = AddRequestIdHeaderMiddleware(get_wsgi_application())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from django.core.management import call_command
-    call_command("runserver", "8000")
+
+    call_command('runserver', '8000')
